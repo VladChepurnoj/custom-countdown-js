978a8a25e238520fc692886a092e6089393e447f
Vlad <chepurnojv@gmail.com>
2021-07-21 19:51:16 +0300

    created 3 countdown div form screens

---
+++ script.js
@@ -0,0 +1,19 @@
+const inputContainer = document.getElementById("input-container");
+const countdownForm = document.getElementById("countdownForm");
+const dateEl = document.getElementById("date-picker");
+
+let countdownTitle = "";
+let countdownDate = "";
+
+const today = new Date().toISOString().split("T")[0];
+dateEl.setAttribute("min", today);
+
+function updateCountdown(e) {
+  e.preventDefault();
+  countdownTitle = e.srcElement[0].value;
+  countdownDate = e.srcElement[1].value;
+
+  console.log(countdownTitle, countdownDate);
+}
+
+countdownForm.addEventListener("submit", updateCountdown);
